6f4dfcc0c4666d30291d229a78378c5589f986b1
Matej Karolcik <matej.karolcik@myposter.de>
2024-01-14 22:42:02 +0100

    make client do parallel requests

--- client/go.mod
+++ client/go.mod
@@ -1,3 +1,5 @@
 module client
 
 go 1.21.5
+
+require github.com/panjf2000/ants/v2 v2.9.0
--- client/main.go
+++ client/main.go
@@ -2,82 +2,109 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
+	"github.com/panjf2000/ants/v2"
 	"io"
 	"mime/multipart"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 )
 
+var (
+	conc        = flag.Int("conc", 1, "number of concurrent requests")
+	numRequests = flag.Int("n", 1, "number of requests")
+)
+
 func main() {
+	flag.Parse()
 	if err := run(); err != nil {
 		panic(err)
 	}
 }
 
 func run() error {
-	url := "http://localhost:3030/render-form"
-
+	endpointUrl := "http://localhost:3030/render-form"
 	modelUrl := "https://jq-staging-matko.s3.eu-central-1.amazonaws.com/gltf/1_p1_duvet-cover_1350x2000.glb"
-
 	client := &http.Client{}
 
-	f, err := os.Open("../testdata/image.jpg")
+	lock := &sync.Mutex{}
+	wg := &sync.WaitGroup{}
+
+	pool, err := ants.NewPoolWithFunc(*conc, func(payload interface{}) {
+		defer wg.Done()
+		start := time.Now()
+		req, err := createRequest(endpointUrl, modelUrl, "../testdata/image.jpg")
+		if err != nil {
+			panic(err)
+		}
+		_, err = client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		lock.Lock()
+		defer lock.Unlock()
+		fmt.Printf("roundtrip time: %s\n", time.Since(start))
+	})
+
 	if err != nil {
-		return fmt.Errorf("reading canvas file: %w", err)
+		return fmt.Errorf("creating pool: %w", err)
+	}
+
+	for i := 0; i < *numRequests; i++ {
+		wg.Add(1)
+		if err = pool.Invoke(struct{}{}); err != nil {
+			panic(err)
+		}
+	}
+
+	wg.Wait()
+	pool.Release()
+
+	return nil
+}
+
+func createRequest(endpointUrl, modelUrl, imagePath string) (*http.Request, error) {
+	f, err := os.Open(imagePath)
+	if err != nil {
+		return nil, fmt.Errorf("reading canvas file: %w", err)
 	}
 
 	buf := new(bytes.Buffer)
 	writer := multipart.NewWriter(buf)
 
 	if err = addField(writer, "model", modelUrl); err != nil {
-		return fmt.Errorf("adding model field: %w", err)
+		return nil, fmt.Errorf("adding model field: %w", err)
 	}
 	if err = addField(writer, "width", "2000"); err != nil {
-		return fmt.Errorf("adding width field: %w", err)
+		return nil, fmt.Errorf("adding width field: %w", err)
 	}
 	if err = addField(writer, "height", "2000"); err != nil {
-		return fmt.Errorf("adding height field: %w", err)
+		return nil, fmt.Errorf("adding height field: %w", err)
 	}
 
 	field, err := writer.CreateFormFile("textures[1]", "canvas.jpg")
 	if err != nil {
-		return fmt.Errorf("creating texture form file: %w", err)
+		return nil, fmt.Errorf("creating texture form file: %w", err)
 	}
 	if _, err = io.Copy(field, f); err != nil {
-		return fmt.Errorf("writing texture file: %w", err)
+		return nil, fmt.Errorf("writing texture file: %w", err)
 	}
 
 	if err = writer.Close(); err != nil {
-		return fmt.Errorf("closing writer: %w", err)
+		return nil, fmt.Errorf("closing writer: %w", err)
 	}
 
-	req, err := http.NewRequest(http.MethodPost, url, buf)
+	req, err := http.NewRequest(http.MethodPost, endpointUrl, buf)
 	if err != nil {
-		return fmt.Errorf("creating request: %w", err)
+		return nil, fmt.Errorf("creating request: %w", err)
 	}
 
 	req.Header.Add("Content-Type", writer.FormDataContentType())
 
-	start := time.Now()
-	resp, err := client.Do(req)
-	if err != nil {
-		return fmt.Errorf("making request: %w", err)
-	}
-
-	fmt.Printf("roundtrip time: %s\n", time.Since(start))
-
-	f, err = os.Create("output.png")
-	if err != nil {
-		return fmt.Errorf("creating output file: %w", err)
-	}
-
-	if _, err = io.Copy(f, resp.Body); err != nil {
-		return fmt.Errorf("writing output file: %w", err)
-	}
-
-	return nil
+	return req, nil
 }
 
 func addField(writer *multipart.Writer, name, content string) error {
